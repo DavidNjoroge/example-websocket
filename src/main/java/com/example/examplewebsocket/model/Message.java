de32df955f84238b66d60f9b52739df20d3c3586
david njoroge <njorogedavid13@gmail.com>
2018-05-09 12:11:05 +0300

    add spring files from spring intializer

---
+++ src/main/java/com/example/examplewebsocket/model/Message.java
@@ -0,0 +1,28 @@
+package com.example.examplewebsocket.model;
+
+public class Message {
+    private String from;
+    private String text;
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public Message(String from, String text) {
+        this.from = from;
+        this.text = text;
+    }
+
+    public String getText() {
+
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
